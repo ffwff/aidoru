e0a1fde27f663627a3aaf8a80e3d8634ed61385a
ffwff <mogamiriver@tuta.io>
2019-03-10 20:44:57 +0700

    PlayerWidget now can play and control media

--- index.py
+++ index.py
@@ -1,6 +1,7 @@
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
+from PyQt5.QtMultimedia import *
 import sys
 
 # base player widget
@@ -14,6 +15,13 @@ class PlayerWidget(QWidget):
     def __init__(self, parent=None, mode=MAIN_MODE):
         QWidget.__init__(self, parent)
 
+        # media player
+        mediaContent = QMediaContent(QUrl(""))
+        self.media = QMediaPlayer()
+        self.media.setMedia(mediaContent)
+        self.mode = mode
+
+        # ui elements
         self.setStyleSheet("""
 QSlider::groove:horizontal {
     height: 6px; /* the groove expands to the size of the slider by default. by giving it a height, it has a fixed size */
@@ -36,19 +44,16 @@ QSlider::handle:horizontal {
         self.setLayout(vboxLayout)
 
         # song info
-        slider = QSlider(Qt.Horizontal)
-        slider.setMinimum(0)
-        slider.setMaximum(1024)
-        slider.setValue(512)
+        self.positionSlider = slider = QSlider(Qt.Horizontal)
         slider.setTracking(True)
         if mode != PlayerWidget.WIDGET_MODE:
             slider.setStyleSheet("margin-top:-6px;")
 
-        albumLabel = QLabel("Album title")
+        self.albumLabel = albumLabel = QLabel("Album title")
         albumLabel.setAlignment(Qt.AlignCenter)
 
         if mode == PlayerWidget.MAIN_MODE:
-            artistLabel = QLabel("Artist name")
+            self.artistLabel = artistLabel = QLabel("Artist name")
             artistLabel.setAlignment(Qt.AlignCenter)
         else:
             albumLabel.setText("Artist title − Artist name")
@@ -63,13 +68,12 @@ QSlider::handle:horizontal {
             coverLabelContainerL.setContentsMargins(0, 0, 0, 0)
             coverLabelContainer.setLayout(coverLabelContainerL)
 
-            coverLabelContainerL.addStretch()
-            coverLabel = QLabel(coverLabelContainer)
+            self.coverLabel = coverLabel = QLabel(coverLabelContainer)
+            coverLabel.setAlignment(Qt.AlignCenter)
             coverLabel.setPixmap(QPixmap.fromImage(QImage("./cover.jpg")).scaledToWidth(300, Qt.SmoothTransformation))
             coverLabel.setMinimumSize(QSize(300, 300))
             coverLabel.setMaximumSize(QSize(300, 300))
             coverLabelContainerL.addWidget(coverLabel)
-            coverLabelContainerL.addStretch()
             vboxLayout.addWidget(coverLabelContainer)
 
             vboxLayout.addWidget(slider)
@@ -93,11 +97,11 @@ QPushButton {
         buttonsLayout = QHBoxLayout(self)
         buttonsWidget.setLayout(buttonsLayout)
 
-        backButton = QPushButton(QIcon.fromTheme("media-skip-backward"), "")
+        self.backButton = backButton = QPushButton(QIcon.fromTheme("media-skip-backward"), "")
 
-        ppButton = QPushButton(QIcon.fromTheme("media-playback-start"), "")
+        self.ppButton = ppButton = QPushButton()
 
-        forwardButton = QPushButton(QIcon.fromTheme("media-skip-forward"), "")
+        self.forwardButton = forwardButton = QPushButton(QIcon.fromTheme("media-skip-forward"), "")
 
         if mode == PlayerWidget.MICRO_MODE:
             vboxLayout.addWidget(buttonsWidget)
@@ -132,13 +136,84 @@ QPushButton {
 
             buttonsLayout.addWidget(slider)
 
-            volumeButton = QPushButton(QIcon.fromTheme("audio-volume-muted"), "")
+            self.volumeButton = volumeButton = QPushButton()
             buttonsLayout.addWidget(volumeButton)
-            volumeSlider = QSlider(Qt.Horizontal)
+            self.volumeSlider = volumeSlider = QSlider(Qt.Horizontal)
             volumeSlider.setStyleSheet("max-width: 100%;")
+            volumeSlider.setMinimum(0)
+            volumeSlider.setMaximum(200)
+            volumeSlider.setValue(100)
             buttonsLayout.addWidget(volumeSlider)
 
             vboxLayout.addStretch()
+
+        # events
+        self.stateChanged(self.media.state())
+        self.mutedChanged(self.media.isMuted())
+        self.bindEvents()
+
+    def bindEvents(self):
+        # song info
+        ## pos slider
+        self.media.durationChanged.connect(self.durationChanged)
+        self.media.positionChanged.connect(self.positionChanged)
+        self.positionSlider.valueChanged.connect(self.positionSliderChanged)
+
+        # controls button
+        self.backButton.clicked.connect(self.backButtonClicked)
+        self.ppButton.clicked.connect(self.ppButtonClicked)
+        self.media.stateChanged.connect(self.stateChanged)
+        self.forwardButton.clicked.connect(self.forwardButtonClicked)
+        if hasattr(self, "volumeButton"):
+            self.volumeButton.clicked.connect(self.volumeButtonClicked)
+            self.media.mutedChanged.connect(self.mutedChanged)
+        if hasattr(self, "volumeSlider"):
+            self.volumeSlider.valueChanged.connect(self.volumeSliderChanged)
+
+    # events
+    ## position slider
+    def durationChanged(self, duration):
+        self.positionSlider.setMaximum(duration)
+
+    def positionChanged(self, position):
+        self.positionSlider.blockSignals(True)
+        self.positionSlider.setValue(position)
+        self.positionSlider.blockSignals(False)
+
+    def positionSliderChanged(self, position):
+        self.media.setPosition(position)
+
+    ## controls
+    def backButtonClicked(self):
+        raise NotImplementedError
+
+    def ppButtonClicked(self):
+        if self.media.state() == QMediaPlayer.PlayingState:
+            self.media.pause()
+        else:
+            self.media.play()
+    def stateChanged(self, state):
+        if state == QMediaPlayer.PlayingState:
+            self.ppButton.setIcon(QIcon.fromTheme("media-playback-pause"))
+        else:
+            self.ppButton.setIcon(QIcon.fromTheme("media-playback-start"))
+
+    def forwardButtonClicked(self):
+        raise NotImplementedError
+
+    ## volume mute
+    def mutedChanged(self, muted):
+        if muted:
+            self.volumeButton.setIcon(QIcon.fromTheme("audio-volume-muted"))
+        else:
+            self.volumeButton.setIcon(QIcon.fromTheme("audio-volume-high"))
+
+    def volumeButtonClicked(self):
+        self.media.setMuted(not self.media.isMuted())
+
+    ## volume slider
+    def volumeSliderChanged(self, volume):
+        self.media.setVolume(volume)
 
 # application widget
 class MediaPlayer(QWidget):
@@ -156,15 +231,15 @@ class MainWindow(QMainWindow):
     def __init__(self):
         QMainWindow.__init__(self)
 
-        #self.setMinimumSize(QSize(1200, 900))
-        self.setMinimumSize(QSize(300, 475))
+        self.setMinimumSize(QSize(1200, 900))
+        #self.setMinimumSize(QSize(300, 475))
         #self.setMinimumSize(QSize(300, 45))
         #self.setMaximumSize(QSize(300, 45))
         self.setWindowTitle("Hello world")
         self.setStyleSheet("background: #fff; color: #000;")
 
-        self.centralWidget = PlayerWidget(self, PlayerWidget.MAIN_MODE)
-        #self.centralWidget = MediaPlayer(self)
+        #self.centralWidget = PlayerWidget(self, PlayerWidget.MAIN_MODE)
+        self.centralWidget = MediaPlayer(self)
         self.setCentralWidget(self.centralWidget)
 
 app = QApplication(sys.argv)
