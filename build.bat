d895f706d230e81482ce127d366e34c1508309c5
ffwff <30137470+ffwff@users.noreply.github.com>
2019-03-19 18:50:17 +0700

    use win32/win64 for architecture name

--- build.bat
+++ build.bat
@@ -2,7 +2,9 @@
 rmdir dist /s /q
 venv\Scripts\activate.bat
 pyinstaller aidoru.spec || goto :error
-powershell -Command "Compress-Archive dist/aidoru aidoru-$ENV:PROCESSOR_ARCHITECTURE.zip"
+if "%PROCESSOR_ARCHITECTURE%" == "x86" set ARCH=win32
+if "%PROCESSOR_ARCHITECTURE%" == "AMD64" set ARCH=win64
+powershell -Command "Compress-Archive dist/aidoru aidoru-$ENV:ARCH.zip"
 exit /b 0
 
 :error
