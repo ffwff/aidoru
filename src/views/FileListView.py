a1433d2f3639f2976c1d42be8342442b477c800b
ffwff <mogamiriver@tuta.io>
2019-03-20 22:14:02 +0700

    Remove spacing from FileListView

--- src/views/FileListView.py
+++ src/views/FileListView.py
@@ -144,6 +144,7 @@ class FileListView(QWidget):
     def initUI(self):
         vboxLayout = QVBoxLayout()
         vboxLayout.setContentsMargins(0,0,0,0)
+        vboxLayout.setSpacing(0)
         self.setLayout(vboxLayout)
 
         self.searchView = SearchView()
