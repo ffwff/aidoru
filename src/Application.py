baa94f18f29ee2077a059523a226e913187e65e3
ffwff <30137470+ffwff@users.noreply.github.com>
2019-03-21 18:06:37 +0700

    wip update feature

--- src/Application.py
+++ src/Application.py
@@ -1,4 +1,6 @@
 from PyQt5.QtWidgets import *
+from urllib.request import urlopen
+import sys
 
 class Application(QApplication):
 
@@ -10,3 +12,17 @@ class Application(QApplication):
         Application.mainWindow = MainWindow()
         Application.mainWindow.initUI()
         return QApplication.exec()
+
+    def update():
+        execPath = os.path.join(os.path.dirname(__file__), "..")
+        if os.path.isdir(os.path.join(execPath, ".git")):
+            def finished(exitCode, exitStatus):
+                python = sys.executable
+                os.execl(python, python, *sys.argv)
+            self.updateProcess = updateProcess = QProcess()
+            update_process.setWorkingDirectory(execPath)
+            update_process.start("git", ["git", "pull"])
+            update_process.finished.connect(finished)
+        elif sys.platform == "win32":
+            latest = urlopen("https://raw.githubusercontent.com/ffwff/aidoru/master/release.txt").read()
+            #print(latest)
