b233cd2bb6b8c06eaca966e6bb7ebb5fd54dadf7
ffwff <mogamiriver@tuta.io>
2019-03-21 12:17:52 +0700

    Add dark theme toggle

--- build.bat
+++ build.bat
@@ -3,9 +3,9 @@ if "%PROCESSOR_ARCHITECTURE%" == "x86" set ARCH=win32
 if "%PROCESSOR_ARCHITECTURE%" == "AMD64" set ARCH=win64
 rmdir dist /s /q
 pyinstaller aidoru.spec || goto :error
-powershell -Command "Compress-Archive dist/aidoru aidoru-$ENV:ARCH.zip"
+powershell -Command "Compress-Archive -Force dist/aidoru aidoru-$ENV:ARCH.zip"
 pyinstaller aidoru-console.spec || goto :error
-powershell -Command "Compress-Archive dist/aidoru aidoru-$ENV:ARCH-console.zip"
+powershell -Command "Compress-Archive -Force dist/aidoru aidoru-$ENV:ARCH-console.zip"
 exit /b 0
 
 :error
