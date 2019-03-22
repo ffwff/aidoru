8af5cd13f588043a66dc3b4d1f788c97b5acc1db
ffwff <30137470+ffwff@users.noreply.github.com>
2019-03-22 23:27:59 +0700

    Update build.bat, release.txt

--- build.bat
+++ build.bat
@@ -2,6 +2,7 @@
 if "%PROCESSOR_ARCHITECTURE%" == "x86" set ARCH=win32
 if "%PROCESSOR_ARCHITECTURE%" == "AMD64" set ARCH=win64
 rmdir dist /s /q
+set CONSOLE=
 pyinstaller aidoru.spec -y || goto :error
 powershell -Command "Compress-Archive -Force dist/aidoru aidoru-$ENV:ARCH.zip"
 set CONSOLE=1
