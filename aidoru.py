6137507231a95563a7243993666a28cb80ebfe67
ffwff <mogamiriver@tuta.io>
2019-03-24 12:48:38 +0700

    chdir to where the program is

--- aidoru.py
+++ aidoru.py
@@ -3,7 +3,9 @@
 from src.Application import Application
 from PyQt5.QtWidgets import *
 import sys
+import os
 
 if __name__ == "__main__":
+    os.chdir(os.path.dirname(os.path.abspath(__file__)))
     app = Application(sys.argv)
     sys.exit(app.exec())
